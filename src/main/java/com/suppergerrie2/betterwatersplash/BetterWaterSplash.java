efe24473275d2f5dd304b22e5fd45dc62f1c4593
suppergerrie2 <gerjan2001@gmail.com>
2019-06-15 19:00:45 +0200

    1.14.2

--- src/main/java/com/suppergerrie2/betterwatersplash/BetterWaterSplash.java
+++ src/main/java/com/suppergerrie2/betterwatersplash/BetterWaterSplash.java
@@ -1,12 +1,12 @@
 package com.suppergerrie2.betterwatersplash;
 
 import java.util.List;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.projectile.EntityPotion;
-import net.minecraft.init.PotionTypes;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.projectile.PotionEntity;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.Potions;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.PotionEffect;
-import net.minecraft.potion.PotionType;
+import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.PotionUtils;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraftforge.event.entity.ProjectileImpactEvent;
@@ -23,26 +23,26 @@ public class BetterWaterSplash {
 
     @SubscribeEvent
     public static void onProjectileImpactEvent(ProjectileImpactEvent.Throwable event) {
-        if (event.getThrowable().world.isRemote || !(event.getThrowable() instanceof EntityPotion)) {
+        if (event.getThrowable().world.isRemote || !(event.getThrowable() instanceof PotionEntity)) {
             return;
         }
 
-        EntityPotion potionEntity = (EntityPotion) event.getThrowable();
+        PotionEntity potionEntity = (PotionEntity) event.getThrowable();
 
-        ItemStack potionItemstack = potionEntity.getPotion();
-        PotionType potiontype = PotionUtils.getPotionFromItem(potionItemstack);
-        List<PotionEffect> potionEffects = PotionUtils.getEffectsFromStack(potionItemstack);
-        boolean isWaterPotion = potiontype == PotionTypes.WATER && potionEffects.isEmpty();
+        ItemStack potionItemstack = potionEntity.getItem();
+        Potion potiontype = PotionUtils.getPotionFromItem(potionItemstack);
+        List<EffectInstance> potionEffects = PotionUtils.getEffectsFromStack(potionItemstack);
+        boolean isWaterPotion = potiontype == Potions.WATER && potionEffects.isEmpty();
 
         if (!isWaterPotion) {
             return;
         }
 
         AxisAlignedBB axisalignedbb = potionEntity.getBoundingBox().grow(4.0D, 2.0D, 4.0D);
-        List<EntityLivingBase> entitiesWithinRange = potionEntity.world.<EntityLivingBase>getEntitiesWithinAABB(EntityLivingBase.class, axisalignedbb);
+        List<LivingEntity> entitiesWithinRange = potionEntity.world.<LivingEntity>getEntitiesWithinAABB(LivingEntity.class, axisalignedbb);
 
         if (!entitiesWithinRange.isEmpty()) {
-            for (EntityLivingBase entitylivingbase : entitiesWithinRange) {
+            for (LivingEntity entitylivingbase : entitiesWithinRange) {
                 double distanceSq = potionEntity.getDistanceSq(entitylivingbase);
 
                 if (distanceSq < 16.0D) {
