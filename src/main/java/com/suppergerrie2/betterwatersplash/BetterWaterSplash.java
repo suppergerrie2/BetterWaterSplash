ffdf93e9a7b5d5c5b0db6d832c698e6d75a12b95
suppergerrie2 <gerjan2001@gmail.com>
2019-10-10 13:35:08 +0200

    Some cleanup and testing for 1.14.4

--- src/main/java/com/suppergerrie2/betterwatersplash/BetterWaterSplash.java
+++ src/main/java/com/suppergerrie2/betterwatersplash/BetterWaterSplash.java
@@ -13,9 +13,7 @@ import net.minecraftforge.event.entity.ProjectileImpactEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(
-    BetterWaterSplash.MOD_ID
-)
+@Mod(BetterWaterSplash.MOD_ID)
 @Mod.EventBusSubscriber(modid = BetterWaterSplash.MOD_ID)
 public class BetterWaterSplash {
 
@@ -23,33 +21,42 @@ public class BetterWaterSplash {
 
     @SubscribeEvent
     public static void onProjectileImpactEvent(ProjectileImpactEvent.Throwable event) {
+        //We only want to know when a potion impacts something, not arrows or other entities.
         if (event.getThrowable().world.isRemote || !(event.getThrowable() instanceof PotionEntity)) {
             return;
         }
 
+        //To make sure it is a water potion we need to check if it is the WATER potion, or if it has no effects at all.
         PotionEntity potionEntity = (PotionEntity) event.getThrowable();
 
         ItemStack potionItemstack = potionEntity.getItem();
-        Potion potiontype = PotionUtils.getPotionFromItem(potionItemstack);
-        List<EffectInstance> potionEffects = PotionUtils.getEffectsFromStack(potionItemstack);
-        boolean isWaterPotion = potiontype == Potions.WATER && potionEffects.isEmpty();
 
+        //Read the potion type and potion effects from the potion ItemStack
+        Potion potionType = PotionUtils.getPotionFromItem(potionItemstack);
+        List<EffectInstance> potionEffects = PotionUtils.getEffectsFromStack(potionItemstack);
+
+        boolean isWaterPotion = (potionType == Potions.WATER && potionEffects.isEmpty());
+
+        //If it isn't a water potion we shouldn't put out the fire.
         if (!isWaterPotion) {
             return;
         }
 
+        //Get the effect radius. TODO: Make distance a config setting?
         AxisAlignedBB axisalignedbb = potionEntity.getBoundingBox().grow(4.0D, 2.0D, 4.0D);
+
+        //Get all entities in the potion's effect radius
         List<LivingEntity> entitiesWithinRange = potionEntity.world.<LivingEntity>getEntitiesWithinAABB(LivingEntity.class, axisalignedbb);
 
+        //Now we need to check if the entities are in a sphere with a radius of 4, and if they are extinguish them.
         if (!entitiesWithinRange.isEmpty()) {
-            for (LivingEntity entitylivingbase : entitiesWithinRange) {
-                double distanceSq = potionEntity.getDistanceSq(entitylivingbase);
+            for (LivingEntity entity : entitiesWithinRange) {
+                double distanceSq = potionEntity.getDistanceSq(entity);
 
                 if (distanceSq < 16.0D) {
-                    entitylivingbase.extinguish();
+                    entity.extinguish();
                 }
             }
         }
     }
-
 }
