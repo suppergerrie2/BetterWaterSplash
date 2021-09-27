0727e8391f23fccab20a00905c20b50e0738dbfc
suppergerrie2 <15769860+suppergerrie2@users.noreply.github.com>
2021-09-27 11:02:38 +0200

    Update to 1.17.1

--- src/main/java/com/suppergerrie2/betterwatersplash/BetterWaterSplash.java
+++ src/main/java/com/suppergerrie2/betterwatersplash/BetterWaterSplash.java
@@ -1,17 +1,16 @@
 package com.suppergerrie2.betterwatersplash;
 
-import java.util.List;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.projectile.PotionEntity;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.Potions;
-import net.minecraft.item.ItemStack;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.PotionUtils;
-import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.ThrownPotion;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.alchemy.*;
+import net.minecraft.world.phys.AABB;
 import net.minecraftforge.event.entity.ProjectileImpactEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
 
 @Mod(BetterWaterSplash.MOD_ID)
 @Mod.EventBusSubscriber(modid = BetterWaterSplash.MOD_ID)
@@ -20,20 +19,18 @@ public class BetterWaterSplash {
     public static final String MOD_ID = "sbetterwatersplash";
 
     @SubscribeEvent
-    public static void onProjectileImpactEvent(ProjectileImpactEvent.Throwable event) {
+    public static void onProjectileImpactEvent(ProjectileImpactEvent event) {
         //We only want to know when a potion impacts something, not arrows or other entities.
-        if (event.getThrowable().world.isRemote || !(event.getThrowable() instanceof PotionEntity)) {
+        if (event.getProjectile().level.isClientSide() || !(event.getProjectile() instanceof ThrownPotion potionEntity)) {
             return;
         }
 
         //To make sure it is a water potion we need to check if it is the WATER potion, or if it has no effects at all.
-        PotionEntity potionEntity = (PotionEntity) event.getThrowable();
-
         ItemStack potionItemstack = potionEntity.getItem();
 
         //Read the potion type and potion effects from the potion ItemStack
-        Potion potionType = PotionUtils.getPotionFromItem(potionItemstack);
-        List<EffectInstance> potionEffects = PotionUtils.getEffectsFromStack(potionItemstack);
+        Potion                  potionType    = PotionUtils.getPotion(potionItemstack);
+        List<MobEffectInstance> potionEffects = PotionUtils.getCustomEffects(potionItemstack);
 
         boolean isWaterPotion = (potionType == Potions.WATER && potionEffects.isEmpty());
 
@@ -43,18 +40,20 @@ public class BetterWaterSplash {
         }
 
         //Get the effect radius. TODO: Make distance a config setting?
-        AxisAlignedBB axisalignedbb = potionEntity.getBoundingBox().grow(4.0D, 2.0D, 4.0D);
+        AABB axisalignedbb = potionEntity.getBoundingBox()
+                                         .inflate(4.0D, 2.0D, 4.0D);
 
         //Get all entities in the potion's effect radius
-        List<LivingEntity> entitiesWithinRange = potionEntity.world.<LivingEntity>getEntitiesWithinAABB(LivingEntity.class, axisalignedbb);
+        List<LivingEntity> entitiesWithinRange = potionEntity.level.getEntitiesOfClass(LivingEntity.class,
+                                                                                       axisalignedbb);
 
         //Now we need to check if the entities are in a sphere with a radius of 4, and if they are extinguish them.
         if (!entitiesWithinRange.isEmpty()) {
             for (LivingEntity entity : entitiesWithinRange) {
-                double distanceSq = potionEntity.getDistanceSq(entity);
+                double distanceSq = potionEntity.distanceToSqr(entity);
 
                 if (distanceSq < 16.0D) {
-                    entity.extinguish();
+                    entity.clearFire();
                 }
             }
         }
